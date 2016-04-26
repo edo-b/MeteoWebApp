d818acc45c02cba7742b93f3c8254e79f4144d1c
Edo <edo.behmen@gmail.com>
2016-04-26 16:49:25 +0200

    added client side validation, error when displaying forecast

--- MeteoWebApp/MeteoWebApp.Web/Content/Scripts/EditForecastScript.js
+++ MeteoWebApp/MeteoWebApp.Web/Content/Scripts/EditForecastScript.js
@@ -5,32 +5,39 @@
         $('#delete-id-field').attr('value', clickedDeleteButton.attr('data'));
     });
     $('.create-button').click(function () {
+        $('#create-validation-failed-warning').html("");
+        $('#create-temperature-field').val(0);
+        $('#create-rain-chance-field').val(0);
+        $('#create-wind-speed-field').val(0);
+        $('#WindDirection').val(1);
+        $('#GeneralState').val(1);
         var clickedCreateButton = $(this);
-        $('#create-date-field').attr('value', clickedCreateButton.attr('data'));
+        $('#create-date-field').val(clickedCreateButton.attr('data'));
         $('#create-date-label').html(clickedCreateButton.attr('data'));
     });
     $('#WindDirection').change(function () {
-        if ($('#WindDirection option:selected').val() == 9) {
-            $('#wind-speed-input-field').attr('value', 0);
-            $('#wind-speed-input-field').attr('disabled', '');
+        if ($('#WindDirection').val() == 9) {
+            $('#create-wind-speed-field').val(0);
+            $('#create-wind-speed-field').attr('disabled', '');
         }
         else {
-            $('#wind-speed-input-field').removeAttr('disabled');
+            $('#create-wind-speed-field').removeAttr('disabled');
         }
     });
     $('.edit-button').click(function () {
+        $('#edit-validation-failed-warning').html("");
         var clickedButton = $(this);
         var id = clickedButton.attr('data');
         $('#edit-date-label').html($('#edit-date-data' + id).attr('data'));
-        $('#edit-id-field').attr('value', clickedButton.attr('data'));
-        $('#edit-temperature-field').attr('value', $('#edit-temperature-data' + id).attr('data'));
-        $('#edit-rain-chance-field').attr('value', $('#edit-rain-chance-data' + id).attr('data'));
-        $('#edit-wind-speed-field').attr('value', $('#edit-wind-speed-data' + id).attr('data'));
+        $('#edit-id-field').val(clickedButton.attr('data'));
+        $('#edit-temperature-field').val($('#edit-temperature-data' + id).attr('data'));
+        $('#edit-rain-chance-field').val($('#edit-rain-chance-data' + id).attr('data'));
+        $('#edit-wind-speed-field').val($('#edit-wind-speed-data' + id).attr('data'));
         var windDirection = $('#edit-wind-direction-data' + id).attr('data');
         $('#WindDirectionEdit').val(windDirection);
         var generalState = $('#edit-general-state-data' + id).attr('data');
         $('#GeneralStateEdit').val(generalState);
-        if($('#edit-wind-speed-field').attr('value') == 0)
+        if($('#edit-wind-speed-field').val == 0)
         {
             $('#edit-wind-speed-field').attr('disabled', '');
         }
@@ -41,12 +48,11 @@
     });
     $('#WindDirectionEdit').change(function () {
         if ($('#WindDirectionEdit option:selected').val() == 9) {
-            $('#edit-wind-speed-field').attr('value', 0);
+            $('#edit-wind-speed-field').val(0);
             $('#edit-wind-speed-field').attr('disabled', '');
         }
         else {
             $('#edit-wind-speed-field').removeAttr('disabled');
         }
     });
-
 });
---
+++ MeteoWebApp/MeteoWebApp.Web/Content/Scripts/ValidateFormScript.js
@@ -0,0 +1,64 @@
+﻿function validateForm(formType) {
+    if (formType == 'create') {
+        var rainChance = $('#create-rain-chance-field').val();
+        var windSpeed = $('#create-wind-speed-field').val();
+
+        var errorString = "";
+        var didErrorOccur = false;
+
+        $('#create-validation-failed-warning').html("");
+
+        if (rainChance < 0 || rainChance > 100) {
+            errorString = "Vjerojatnost kiše mora biti od 0 do 100";
+            didErrorOccur = true;
+        }
+        if (windSpeed < 0 || windSpeed > 100) {
+            if (errorString == "") {
+                errorString = "Brzina vjetra mora biti veća od 0";
+                didErrorOccur = true;
+            }
+            else {
+                errorString += ", brzina vjetra mora biti veća od 0";
+                didErrorOccur = true;
+            }
+        }
+        if (didErrorOccur == true) {
+            var warningHtml = "<div class=\"alert alert-danger\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Greška!</strong> " + errorString + ".</div>";
+            $('#create-validation-failed-warning').html(warningHtml);
+            return false;
+        }
+
+        return true;
+    }
+    else if (formType == 'edit') {
+        var rainChance = $('#edit-rain-chance-field').val();
+        var windSpeed = $('#edit-wind-speed-field').val();
+
+        var errorString = "";
+        var didErrorOccur = false;
+
+        $('#edit-validation-failed-warning').html("");
+
+        if (rainChance < 0 || rainChance > 100) {
+            errorString = "Vjerojatnost kiše mora biti od 0 do 100";
+            didErrorOccur = true;
+        }
+        if (windSpeed < 0 || windSpeed > 100) {
+            if (errorString == "") {
+                errorString = "Brzina vjetra mora biti veća od 0";
+                didErrorOccur = true;
+            }
+            else {
+                errorString += ", brzina vjetra mora biti veća od 0";
+                didErrorOccur = true;
+            }
+        }
+        if (didErrorOccur == true) {
+            var warningHtml = "<div class=\"alert alert-danger\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Greška!</strong> " + errorString + ".</div>";
+            $('#edit-validation-failed-warning').html(warningHtml);
+            return false;
+        }
+
+        return true;
+    }
+}
